d4e5533a9375abe56677b3074977f2621ceedeec
Amajuoyi chidera <Amajuoyichideradavid@gmail.com>
2023-04-28 16:23:25 -0700

    first version

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,13 +9,13 @@ const postList = ({postData}) => {
         <title> My Task </title>
     </Head>
 
-    <h1 className="text-5xl pb-10 pt-10">My Posts</h1>
+    <h1 className="text-3xl pb-10 pt-10 font-medium">My Posts</h1>
 
     {postData.map((post) => {
       return (
         <div className="myPost">
-          <h3 className="text-2xl pt-3 pb-3"> {post.id} </h3>
-          <h1 className="text-3xl pt-4 pb-4"> {post.title} </h1>
+          <h3 className="text-xl pt-3 pb-3 font-medium"> {post.id} </h3>
+          <h1 className="text-xl pt-4 pb-4 font-medium"> {post.title} </h1>
           <p className="pt-3 pb-3"> {post.body} </p>
         </div>
       )
